8e461be15de797a7f2dd59e9fc6cbb82c0632a0d
imakash665 <imakash665@gmail.com>
2025-06-21 14:08:00 +0530

    add v5

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Login to AWS ECR
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 869935088600.dkr.ecr.us-east-1.amazonaws.com
-docker pull 869935088600.dkr.ecr.us-east-1.amazonaws.com/skyx-ecr:v4
+docker pull 869935088600.dkr.ecr.us-east-1.amazonaws.com/skyx-ecr:v5
 
 if [ "$(docker ps -q -f name=skyx-app)" ]; then
     docker stop skyx-app || true
@@ -11,4 +11,4 @@ if [ "$(docker ps -aq -f name=skyx-app)" ]; then
     docker rm skyx-app || true
 fi
 
-docker run -d -p 80:5000 -e DAGSHUB_PAT=eac8585ad4083e279c466bd0514fffdf07054e30 --name skyx-app 869935088600.dkr.ecr.us-east-1.amazonaws.com/skyx-ecr:v4
+docker run -d -p 80:5000 -e DAGSHUB_PAT=eac8585ad4083e279c466bd0514fffdf07054e30 --name skyx-app 869935088600.dkr.ecr.us-east-1.amazonaws.com/skyx-ecr:v5
